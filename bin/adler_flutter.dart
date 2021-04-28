f9cb9b543258c3730c4febcb48f5be1975871be6
AdlerCachuba <adlercachuba@gmail.com>
2021-04-28 20:43:55 -0300

    first commit

---
+++ bin/adler_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:adler_flutter/adler_flutter.dart' as adler_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${adler_flutter.calculate()}!');
+}
---
+++ test/adler_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:adler_flutter/adler_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
