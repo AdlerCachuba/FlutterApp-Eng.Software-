5aab03f9555304404ff9e07cd7741d152d2ece0a
AdlerCachuba <adlercachuba@gmail.com>
2021-07-09 18:40:24 -0300

    exclusão de item

--- lib/app/view/obra_list.dart
+++ lib/app/view/obra_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adler/app/database/databaseConnect.dart';
+import 'package:flutter_adler/app/domain/model/newObra.dart';
+import 'package:flutter_adler/obra_list_back.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class ObraList  extends StatefulWidget {
   @override
@@ -7,18 +10,40 @@ class ObraList  extends StatefulWidget {
 }
 
 class _ObraListState extends State<ObraList> {
-  var _list = [];
 
-  Future trazerLista() async{
-    var retorno  = await DatabaseConnect.instance.buscarTodasObras();
-    setState(() {
-      _list = retorno;
-    });
+  final _back = ObraListBack();
+
+  CircleAvatar avatar(String url){
+    try{
+      return CircleAvatar(backgroundImage: NetworkImage(url));
+    }catch(e){
+      return CircleAvatar(child: Icon(Icons.person));
+    }
   }
-  @override
-  void initState() {
-    trazerLista();
-    super.initState();
+
+  Widget editarButton(Function edit){
+    return IconButton(onPressed: edit, icon: Icon(Icons.edit),color: Colors.deepOrange);
+  }
+
+  Widget remover(BuildContext context, Function remove){
+    return IconButton(
+      icon: Icon(Icons.delete),
+      color: Colors.red,
+      onPressed: (){
+        showDialog(context: context,
+            builder: (context)=>AlertDialog(
+              title: Text("Excluir"),
+              content: Text("Confirma Exclusão?"),
+              actions: [
+                FlatButton(onPressed: (){
+                  Navigator.of(context).pop();
+                }, child: Text("Não")),
+                FlatButton(onPressed: remove, child: Text("Sim")),
+              ],
+            ));
+      },
+
+    );
   }
 
   @override
@@ -34,24 +59,45 @@ class _ObraListState extends State<ObraList> {
                 })
           ],
         ),
-        body: ListView.builder(
-          itemCount: _list.length,
-          itemBuilder: (context, i) {
-            final avatar = _list[i].foto.isEmpty  ? CircleAvatar(child: Icon(Icons.person)) : CircleAvatar(  backgroundImage: NetworkImage(_list[i].foto));
-            return ListTile(
-              leading: avatar,
-              title: Text(_list[i].nome),
-              subtitle: Text(_list[i].quantidade.toString()),
-              trailing: Container(
-                width: 100,
-                child: Row(
-                  children: [
-                    IconButton(icon: Icon(Icons.edit), onPressed: null),
-                    IconButton(icon: Icon(Icons.delete), onPressed: null),
-                  ],
-                ),
-              ),
-            );
+        body: Observer(
+          builder: (context) {
+            return FutureBuilder(
+                future: _back.list,
+                builder: (context, index) {
+                  if (!index.hasData) {
+                    return CircularProgressIndicator();
+                  } else {
+                    List<NewObra> _list = index.data;
+                    return ListView.builder(
+                      padding: const EdgeInsets.all(2.0),
+                      itemCount: _list.length,
+                      itemBuilder: (context, i) {
+                        return ListTile(
+                          leading: avatar(_list[i].foto),
+                          title: Text(
+                            _list[i].nome,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(_list[i].quantidade.toString()),
+                          trailing: Container(
+                            width: 100,
+                            child: Row(
+                              children: [
+                                editarButton((){_back.goToForm(context,_list[i]);}),
+                                remover(context,(){
+                                  _back.remove(_list[i].id);
+                                  Navigator.of(context).pop();
+                                }),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                });
           },
         ));
   }
